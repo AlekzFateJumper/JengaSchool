6bb7616c1c744ab14263907f03063a83fcd747da
Alexandre <alexandre@aeonforce.com>
2023-05-17 20:43:24 -0300

    Buttons working + better orbit

--- Assets/Scripts/CameraOrbit.cs
+++ Assets/Scripts/CameraOrbit.cs
@@ -4,22 +4,37 @@ using UnityEngine;
 
 public class CameraOrbit : MonoBehaviour {
 
-    public GameObject Alvo;
+
+    public List<GameObject> Alvos;
     public int Tempo;
 
+    private GameObject Alvo;
+    private int index = 1;
     private float VelocidadeHorizontal = 100f;
     private float VelocidadeVertical = 60f;
 
+
+    void Start(){
+        Alvo = Alvos[index];
+    }
+
     void Update ()
     {
-        transform.LookAt(Alvo.transform);
+        Vector3 lilUp = Alvo.transform.position + new Vector3(0, 3, 0);
+        transform.LookAt(lilUp, Vector3.up);
         if(Input.GetMouseButton(0)){
             float zs = Mathf.Sign(transform.position.z);
             var VelHorizontal = VelocidadeHorizontal * Input.GetAxis("Mouse X");
             var VelVertical = VelocidadeVertical * Input.GetAxis("Mouse Y") * zs;
             if( transform.position.y > 1.5f || ( zs * VelVertical < 0 )){
-                transform.RotateAround(Alvo.transform.position, new Vector3(VelVertical, VelHorizontal, 0), Tempo * Time.deltaTime);
+                transform.RotateAround(lilUp, new Vector3(VelVertical, VelHorizontal, 0), Tempo * Time.deltaTime);
             }
         }
+    }
+
+    public void SetTarget(int step){
+        index = ( index + step ) % Alvos.Count;
+        Alvo = Alvos[index];
+        transform.position = new Vector3(Alvo.transform.position.x, 5, -11);
     }
 }
